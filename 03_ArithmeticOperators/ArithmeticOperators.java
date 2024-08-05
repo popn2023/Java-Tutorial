1a8533151ac9cdf7d6d33cc3557db8364499e74a
popn2023 <popn@vt.edu>
2024-08-05 08:30:10 -0400

    added arithmetic operators files

---
+++ 03_ArithmeticOperators/ArithmeticOperators.java
@@ -0,0 +1,39 @@
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+        
+        int zero = 0;
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int five = 5;
+
+        // addition (+)
+        System.out.println("zero + one = " + (zero + one));
+        System.out.println("two + three = " + (two + three));
+
+        // subtraction (-)
+        System.out.println("five - four = " + (five - four));
+        System.out.println("one - three = " + (one - three));
+
+        // multiplication (*)
+        System.out.println("zero * two = " + (zero * two));
+        System.out.println("two * five = " + (two * five));
+
+        // division (/)
+        System.out.println("four / two = " + (four / two));
+        System.out.println("four / five = " + (four / five));
+        System.out.println("five / four = " + (five / four));
+        System.out.println("five / two = " + (five / two));
+        // System.out.println(one / zero);
+
+        // modulous (%)
+        System.out.println("four % two = " + (four % two));
+        System.out.println("four % five = " + (four % five));
+        System.out.println("five % four = " + (five % four));
+        System.out.println("five % two = " + (five % two));
+
+    }
+
+}
